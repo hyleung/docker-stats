ca76c7d972ffa88f6cfb544878413660bc851e1d
Ho Yan Leung <hy.leung@gmail.com>
2016-07-24 08:23:30 -0700

    LineChart renders data range with offset

--- cpustats.go
+++ cpustats.go
@@ -28,6 +28,7 @@ func NewCpuUsageWidget() *CpuUsageWidget {
 	cpuGraph.BorderLabel = "CPU Usage"
 	cpuGraph.Height = 10
 	cpuGraph.PaddingTop = 1
+	cpuGraph.PaddingRight = 10
 	cpuGraph.Mode = "dot"
 	cpuGraph.X = 0
 	cpuGraph.Y = 0
@@ -43,17 +44,31 @@ func NewCpuUsageWidget() *CpuUsageWidget {
 		var cpuPct = 0.0
 		cpuPct, currentCPUUsage, currentSystemUsage = computeCpu(stats, currentCPUUsage, currentSystemUsage)
 		cpuHistory[cpuHead] = cpuPct * 100.0
-		if cpuHead < 599 {
+		if cpuHead < len(cpuHistory) {
 			cpuHead = cpuHead + 1
 		} else {
 			cpuHead = 0
+			//reset the data range
+			cpuHistory = make([]float64, 600)
 		}
 		cpuUsage.Text = fmt.Sprintf("CPU Usage: %5.2f%%", cpuPct*100)
-		cpuGraph.Data = cpuHistory[:i]
+		cpuGraph.Data = getDataRange(cpuGraph, cpuHistory, cpuHead)
 		i = i + 1
 	}}
 }
 
+func computeNumPoints(lc *ui.LineChart) int {
+	padding := 20
+	return (lc.Width - padding)
+}
+func getDataRange(lc *ui.LineChart, data []float64, head int) []float64 {
+	points := computeNumPoints(lc)
+	lc.BorderLabel = fmt.Sprintf("CPU Usage: head = %d, points = %d", head, points)
+	if head < points {
+		return data
+	}
+	return data[(head - points):]
+}
 func computeCpu(stats types.StatsJSON, currentUsage uint64, currentSystemUsage uint64) (cpuPct float64, cpuUsage uint64, systemUsage uint64) {
 	//compute the cpu usage percentage
 	//via https://github.com/docker/docker/blob/e884a515e96201d4027a6c9c1b4fa884fc2d21a3/api/client/container/stats_helpers.go#L199-L212
